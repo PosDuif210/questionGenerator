b52257017c860f7eba5a02eda92b8f41436e7ff3
PosterBoycott <thinus5422@gmail.com>
2023-07-11 18:01:29 +0200

    Include Change

--- test.cpp
+++ test.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <fstream>
+#include<string>
 
 int main ()
 {
     std::fstream file("good.txt", std::ios::in | std::ios::out);
     if (file.is_open()){
+        std::string tp;
         std::cout << "File opened" << std::endl;
+        std::getline(file, tp);
+        std::cout << tp << std::endl;
     }
     else{
         std::cout << "File did not open"<< std::endl;
     }
+
 
 
 }
